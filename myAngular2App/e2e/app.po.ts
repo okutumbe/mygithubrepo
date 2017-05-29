6c16c365e73dfafe094d7d52c9d5515cd9aad4a0
Omkar <okutumbe@gmail.com>
2017-05-29 21:44:14 +0530

    Added sample UI app

---
+++ myAngular2App/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class MyAngular2AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
